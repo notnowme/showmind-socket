6f7f7c149dc063a62ebf9accfbac34b2926cde9f
sole <notnowyou@naver.com>
2024-03-04 21:04:06 +0900

    선 공유 소켓 추가

--- src/app.ts
+++ src/app.ts
@@ -1,14 +1,19 @@
 import express from "express";
 import http from 'http';
+import fs from 'fs';
 import { Server as SocketIO} from 'socket.io';
 import cors from 'cors';
- 
+const options = {
+    key: fs.readFileSync('./config/cert.key'),
+    cert: fs.readFileSync('./config/cert.crt')
+};
+
 const app = express();
 
 app.use(cors({
     origin: '*'
 }));
-const server = http.createServer(app);
+const server = http.createServer( app);
 export const io = new SocketIO(server, {
     cors: {
         origin: '*'
@@ -67,4 +72,17 @@ io.on('connection', (socket) => {
         const { rid, msg, user } = data;
         io.to(rid).emit('msg', {msg, user});
     });
+
+    socket.on('drawup', (data, cb) => {
+        const { rid, curLine} = data;
+        cb({
+            ok: true,
+        })
+        io.to(rid).emit('sendLine', curLine);
+    });
+
+    socket.on('drawNew', (data) => {
+        const { rid } = data;
+        io.to(rid).emit('newLine');
+    })
 });
